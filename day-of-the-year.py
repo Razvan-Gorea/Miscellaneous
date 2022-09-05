25a9b418e5e85d1de0ef73dd3431f2048404d28c
Razvan Gorea <103372955+Razvan-Gorea@users.noreply.github.com>
2022-09-05 16:40:15 +0100

    Delete day-of-the-year.py

--- day-of-the-year.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-month_num = int(input("Give me the month number: "))
-day_num = int(input("Give me the day number: "))
-day_of_year = ((month_num - 1) * 30) + day_num
-print(f'Day of year: {day_of_year}')

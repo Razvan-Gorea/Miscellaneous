856757896f46deb85328c00dd5ae250db8b88585
Razvan Gorea <103372955+Razvan-Gorea@users.noreply.github.com>
2022-09-05 16:46:55 +0100

    Delete gaelic-score.py

--- gaelic-score.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-goals = int(input("Number of goals: "))
-points = int(input("Number of points: "))
-
-goals = goals * 3
-
-gaelic_score = goals + points
-print(f'Gaelic Score: {gaelic_score}')

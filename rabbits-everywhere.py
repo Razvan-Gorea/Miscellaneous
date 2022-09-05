2309697a1a48214b6ee971d1d3b34065063854a3
Razvan Gorea <103372955+Razvan-Gorea@users.noreply.github.com>
2022-09-05 16:47:46 +0100

    Delete rabbits-everywhere.py

--- rabbits-everywhere.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-start_pop = int(input("Give the initial rabbit population: "))
-num_of_years = int(input("Number of years of reproduction: "))
-final_pop = start_pop * (2 ** num_of_years)
-print(f'Final rabbit population: {final_pop}')

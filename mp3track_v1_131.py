97e6af4620994445f992f26fb3eee921976a2af5
Razvan Gorea <razvan.gorea2@mail.dcu.ie>
2023-05-16 14:00:22 +0100

    Removed unnecessary files from branch

--- mp3track_v1_131.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-class MP3Track(object):
-  def __init__(self, title, duration):
-    self.title = title
-    self.duration = duration
-
-  def __str__(self):
-    return 'Title: {}\nDuration: {}'.format(self.title, self.duration)
